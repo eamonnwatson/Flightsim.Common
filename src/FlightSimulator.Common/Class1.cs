67c75c39f96f46f352ac4053f86781fec0564409
Eamonn Watson <eamonn.watson@flyacva.com>
2021-02-28 22:31:39 -0400

    Change of Direction

---
+++ src/FlightSimulator.Common/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace FlightSimulator.Common
+{
+    class Class1
+    {
+        public void Test1()
+        {
+            var test = new OffsetDictionary();
+
+            test.AddOffset<int>("TIME", "main", 0x2345);
+
+        }
+
+    }
+}
---
+++ src/FlightSimulator.Common/OffsetDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSUIPC;
+using UnitsNet;
+
+namespace FlightSimulator.Common
+{
+    class OffsetDictionary
+    {
+        private readonly Dictionary<string, Offset> offsets = new Dictionary<string, Offset>();
+
+        internal void AddOffset<TOffset>(string name, string groupName, int address)
+        {
+            var offset = new Offset<TOffset>(groupName, address);
+            offsets.Add(name, offset);
+        }
+
+
+
+    }
+}
